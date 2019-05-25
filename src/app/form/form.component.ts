542b6c6b6971aae2d93732421e2f3a4ee9efb83e
Paweł Sadownik <pwl.sadownik@gmail.com>
2019-05-25 20:01:41 +0200

    form Component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form',
@@ -11,5 +13,15 @@ export class FormComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  createFormGroup() {
+    return new FormGroup({
+      personalData: new FormGroup({
+        email: new FormControl(),
+        mobile: new FormControl(),
+        country: new FormControl()
+       }),
+      requestType: new FormControl(),
+      text: new FormControl()
+    });
+  }
 }
