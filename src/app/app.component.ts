22533440e6fc83e955fcafe44de9093a977a26c5
Paweł Sadownik <pwl.sadownik@gmail.com>
2019-05-25 19:58:39 +0200

    defining a model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,27 @@ import {from} from 'rxjs/internal/observable/from';
 })
 export class AppComponent {
   title = 'reactiveForms';
+
+  presonalData: RegisteredUser
+  requestType: any = ''
+  text: string=''
+}
+
+interface RegisteredUser {
+name: string; // Imię
+surname: string; // Nazwisko
+email: string;  // E-mail
+phone: string | null; // Numer telefonu
+password: string; // Hasło
+pet: 'dog' | 'cat' | 'other'; // Zwierzę
+address: {
+  city: string; // Miasto
+  street: string; // Ulica
+  building: string; // Numer domu
+  flatNo: string | null; // Numer mieszkania
+};
+consents: {
+  newsletter: boolean; // Zgoda na otrzymywanie wiadomości e-mail.
+  ms: boolean; // Zgoda na otrzymywanie wiadomości SMS.
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
